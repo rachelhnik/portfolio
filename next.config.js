964b7cd81d70868dc01713e08352d0ed28342a3d
rachel <shwehnit14@gmail.com>
2024-08-29 17:32:07 +0630

    fix: add yml

---
+++ next.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  output: "export",
+  basePath: process.env.NODE_ENV === "production" ? "" : undefined,
+  experimental: {
+    appDir: true,
+  },
+  images: {
+    unoptimized: true,
+  },
+  reactStrictMode: true,
+};
